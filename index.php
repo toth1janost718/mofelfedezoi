f46cfa3d655549bea95be050c978288d5d88874f
Toth Tamas <tothjanostamas@gmail.com>
2020-04-10 00:33:24 +0200

    Test 2.0 index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+
+<html>
+<?php
+include_once 'include/head.php';
+?>
+
+<body class="is-preload">
+
+
+	<!-- Wrapper -->
+	<div id="wrapper">
+
+		<!-- Main -->
+		<div id="main">
+			<div class="inner">
+
+				<?php
+				include_once 'include/header.php';
+				?>
+
+				<?php
+				include_once 'include/banner.php';
+				?>
+
+				<?php
+				include_once 'include/news.php';
+				?>
+
+			</div>
+		</div>
+
+		<?php
+		include_once 'include/sidebar.php';
+		?>
+
+	</div>
+
+
+
+	<?php
+	include_once 'include/script.php'; ?>
+	<script src="../../assets/js/basicpage.js"></script>
+
+
+
+</body>
+
+</html>
